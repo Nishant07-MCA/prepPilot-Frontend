54298a614f72aa9c66ba5938e413a559b5431a0e
Nishant Kumar <nishkumar774@gmail.com>
2026-09-17 17:49:09 +0530

    fix: point api to live render backend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://preppilot-backend-3z49.onrender.com',
 });
 
 // Auto attach JWT token agar user logged in ho
